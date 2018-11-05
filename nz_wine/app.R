91440fee4cee8f93f62b142248747a574951f15f
Meaghan Townsend <meaghan.e.townsend@gmail.com>
2018-11-05 14:48:11 -0500

    last part of class

--- nz_wine/app.R
+++ nz_wine/app.R
@@ -1,19 +1,17 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+library(tidyverse)
+library(readxl)
+
+nz_wines <- read_rds("nz_wine/wine.rds")
+
+View(nz_wines)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("New Zealand Wine"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
@@ -36,12 +34,15 @@ ui <- fluidPage(
 server <- function(input, output) {
    
    output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      wineries <- nz_wines %>%
+       gather() %>%
+       filter(!is.na(ENROLLMENT_12)) %>%
+       ggplot(aes(x = ENROLLMENT_12)) +
+       geom_histogram(binwidth = input$bins + 1) +
+       labs(title = "Frequency of Enrollment in Infant Care Centers",
+            x = "Total Center Enrollment of 12-Month Center")
+     
+      nz_wines
    })
 }
 
